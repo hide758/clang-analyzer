36c84e756b55f5c1bbe891b8e839f2d87485ba29
guny <pc-van@nifty.com>
2025-02-04 11:24:26 +0000

    migrate database

--- app/FunctionServey/models.py
+++ app/FunctionServey/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Function(models.Model):
+    """Function Model
+    """
+
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    return_type = models.TextField()
+    arguments = models.JSONField()
+    file = models.TextField()
+    line = models.IntegerField()
+
+    static = models.BooleanField(default=False)
+    const = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'function'
+
+    def __str__(self):
+        return f"{self.name}"
+
+class FunctionRelation(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    call_from = models.ForeignKey(
+        Function,
+        on_delete=models.CASCADE,
+        related_name='call_from')
+    
+    call_to = models.ForeignKey(
+        Function,
+        on_delete=models.CASCADE,
+        related_name='call_to')
+
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'function_relation'
+
+    def __str__(self):
+        return f"{self.call_from} -> {self.call_to}"
