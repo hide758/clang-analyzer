aac8eba3d5b697dd902eaa676f02e46c29367c02
guny <pc-van@nifty.com>
2025-02-06 02:56:48 +0000

    modify typo

--- app/FunctionSurvey/apps.py
+++ app/FunctionSurvey/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FunctionserveyConfig(AppConfig):
+class FunctionsurveyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FunctionServey'
+    name = 'FunctionSurvey'
