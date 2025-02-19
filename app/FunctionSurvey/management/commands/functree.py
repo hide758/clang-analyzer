17721c31e23480ea885280d3a24ac1252819c008
guny <pc-van@nifty.com>
2025-02-19 02:28:40 +0000

    add display function tree feature

--- app/FunctionSurvey/management/commands/functree.py
+++ app/FunctionSurvey/management/commands/functree.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         "lower" : []
     }
 
-    def _TraceUpperTree(self, FunctionName:str="", Depth:int=1):
+    def _TraceFunctionTree(self, FunctionName:str="", Depth:int=1):
         """trace function tree (upper)
 
         Args:
@@ -43,9 +43,10 @@ class Command(BaseCommand):
                 "next": [],
             }
 
+            # get upper functions
             upper_functions = self._ProjectFunctionRelation.filter(call_to__name = FunctionName).values_list("call_from__name", flat=True).distinct()
             for func_name in list(upper_functions):
-                NextElement = self._TraceUpperTree(FunctionName = func_name, Depth=Depth - 1)
+                NextElement = self._TraceFunctionTree(FunctionName = func_name, Depth=Depth - 1)
                 TreeElement["next"].append(NextElement)
         
 
@@ -56,15 +57,24 @@ class Command(BaseCommand):
                 "next": [],
             }
 
+            # get lower functions
             lower_functions = self._ProjectFunctionRelation.filter(call_from__name = FunctionName).values_list("call_to__name", flat=True).distinct()
             for func_name in list(lower_functions):
-                NextElement = self._TraceUpperTree(FunctionName = func_name, Depth=Depth + 1)
+                NextElement = self._TraceFunctionTree(FunctionName = func_name, Depth=Depth + 1)
                 TreeElement["next"].append(NextElement)
  
 
 
         return TreeElement
         
+    def _DisplayFunctionTree(self, FunctionTree:dict, depth = 0):
+
+        DisplayFunction = self._ProjectFunction.filter(name = FunctionTree['name']).first()
+        logger.info(f"{"\t" * depth} {"static " if DisplayFunction.static else ""}{DisplayFunction.return_type} {DisplayFunction.name}")
+
+        for func in FunctionTree["next"]:
+            self._DisplayFunctionTree(FunctionTree = func, depth = depth + 1)
+        pass
 
 
     def handle(self, *args, **options):
@@ -75,16 +85,29 @@ class Command(BaseCommand):
             start_time = datetime.datetime.now()
 
 
+            # display start up infomation
             logger.info("Function Tree Start")
             logger.info(f" {start_time.strftime('%Y/%m/%d %H:%M:%S')}")
+            logger.info(f"Target project : {options['project']}")
+            logger.info(f"Target function : {options['target-function']}")
             
             # select project data
             self._ProjectFunction = Function.objects.filter(project__name=options["project"])
             self._ProjectFunctionRelation = FunctionRelation.objects.filter(project__name=options["project"])
 
             # trace function tree
-            self._FunctionTree["upper"] = self._TraceUpperTree(FunctionName = options["target-function"], Depth = options["upper"])
-            self._FunctionTree["lower"] = self._TraceUpperTree(FunctionName = options["target-function"], Depth = -options["lower"])
+            self._FunctionTree["upper"] = self._TraceFunctionTree(FunctionName = options["target-function"], Depth = options["upper"])
+            self._FunctionTree["lower"] = self._TraceFunctionTree(FunctionName = options["target-function"], Depth = -options["lower"])
+            
+            # display function tree
+            if len(self._FunctionTree["upper"]["next"]) > 0:
+                logger.info("** Upper Function Tree")
+                self._DisplayFunctionTree(FunctionTree = self._FunctionTree["upper"])
+
+            if len(self._FunctionTree["lower"]["next"]) > 0:
+                logger.info("** Lower Function Tree")
+                self._DisplayFunctionTree(FunctionTree = self._FunctionTree["lower"])
+
             pass
 
             
